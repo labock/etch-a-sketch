6a612988c6672ea5e5a74f8580cd0addb5eac7da
Luke Bock <lbock@example.com>
2016-10-31 13:11:21 -0500

    Scalability, color randomization

--- js/main.js
+++ js/main.js
@@ -1,17 +1,51 @@
+var gridSize = 16;
+var color = "black";
+
 $(document).ready(function(){
-	for (var i = 0; i < 16; i++) {
-		var div = document.createElement("div");
-		div.style.height = "100px";
-		div.style.width  = "100px";
-		div.style.background = "red";
-		// div.style.margin = "5px";  --good for initial visualization
-		div.style.float = "left";
-		if(i%4 == 0){
-			div.style.clear = "both";
-		}
-		$('.container').append(div);
-	};
-	$('div').mouseenter(function(){
-		this.style.background = "black";
-	});
+	initGrid(+gridSize);
+	$('button').on('click', function(event) {
+    event.preventDefault();
+  })
 });
+
+var resizeGrid = function(){
+	gridSize = prompt("Enter new grid size: ", gridSize);
+	$('.container').empty();
+	initGrid(+gridSize)
+}
+var generateSquares = function(n){
+	var squares = ''
+	for (var i = 0; i < gridSize; i++) {
+		for (var j = 0; j < gridSize; j++) {
+			squares += '<div class="square"></div> ';
+		}
+	}
+	return squares;
+}
+
+var setSize = function(n){
+	var width = ($('.container').width() / parseFloat(n));
+    var height = ($('.container').height() / parseFloat(n));
+    return {'height' : height, 'width' : width};
+}
+
+var setColor = function(color){
+	if (color === 'random') {
+    	return randomColor();
+  	}
+  	else {
+    	return color;
+  	}
+}
+
+var randomColor = function() {
+  return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
+}
+
+var initGrid = function(n){
+	$('.container').append(generateSquares(n));
+	$('.square').css(setSize(n));
+	$('.square').hover(function(){
+		$(this).css({'background-color' : setColor(color)});
+	});
+}
